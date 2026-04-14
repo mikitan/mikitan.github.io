6ed150ff4a7d9d478a7b189044f4fdf2692b88c2
micvictory-asset-node <38480316+mikitan@users.noreply.github.com>
2026-04-14 20:05:29 +0900

    main.py の作成

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # これがないとサイトから接続できません
+
+@app.route('/v1/predict')
+def predict():
+    return jsonify({
+        "status": "success",
+        "prediction": {
+            "signal": "Strong Buy",
+            "accuracy": "94.2%",
+            "next_move": "上昇トレンド継続"
+        }
+    })
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
